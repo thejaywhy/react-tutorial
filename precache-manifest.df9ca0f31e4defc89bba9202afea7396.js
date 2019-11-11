89f2059af2b9310205f2d8b2d28e7769d38aa78b
Jon Young <jaywhyvt@gmail.com>
2019-11-11 14:27:33 -0500

    Updates

---
+++ precache-manifest.df9ca0f31e4defc89bba9202afea7396.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f07bc237c94520183147682fa915a0e9",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "05312a13e4283ee284be",
+    "url": "/react-tutorial/static/css/main.71bd5e9e.chunk.css"
+  },
+  {
+    "revision": "34e30f6b7bd5ad11cf82",
+    "url": "/react-tutorial/static/js/2.9b6246ed.chunk.js"
+  },
+  {
+    "revision": "05312a13e4283ee284be",
+    "url": "/react-tutorial/static/js/main.e9968cf4.chunk.js"
+  },
+  {
+    "revision": "23cd54c0b15ae01a9f16",
+    "url": "/react-tutorial/static/js/runtime-main.09266777.js"
+  }
+]);
